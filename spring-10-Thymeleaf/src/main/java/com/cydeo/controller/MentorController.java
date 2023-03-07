81811af5905902205235c76da77061965df1ac15
Ivan_Cydeo <remezivan1@gmail.com>
2023-03-07 15:29:05 -0500

    Thymeleaf completed

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -1,21 +1,47 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
 
-    @RequestMapping("/register")    // localhost:8080/mentor/register
-    public String register() {
+    @GetMapping("/register")    // localhost:8080/mentor/register
+    public String register(Model model) {
 
-        return "student/register";
+        List<String> batchList = Arrays.asList("JD1", "JD2", "JD3");
+        model.addAttribute("batchList", batchList);
+
+        model.addAttribute("mentor", new Mentor());
+        // ^^ adds new Mentor object (id="mentor") to the Model
+
+        return "mentor/mentor-register";
     }
 
-    @RequestMapping("/drop")    // localhost:8080/mentor/drop
-    public String drop() {
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor) {
 
-        return "student/register";
+//        return "mentor/mentor-confirmation";
+        // how can I access "mentor" attribute in this method?
+        // -> @ModelAttribute
+//        model.addAttribute("mentor", new Mentor());
+//
+//        return "mentor/mentor-register";
+
+// FUTURE: save Mentor Object in DB
+
+        return "redirect:/mentor/register";
+
     }
+
 }
--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,15 +3,14 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register")    // localhost:8080/student/register
+    @RequestMapping(value = "/register", method = RequestMethod.GET)    // localhost:8080/student/register
+    @GetMapping("/register")    // SAME AS ^^^
     public String register(Model model) {
 
         model.addAttribute("students", DataGenerator.createStudent());
@@ -19,6 +18,7 @@ public class StudentController {
     }
 
     @RequestMapping("/welcome")    // localhost:8080/student/welcome?name=Ozzy
+    @PostMapping("/welcome")
     public String welcome() {
 
 
