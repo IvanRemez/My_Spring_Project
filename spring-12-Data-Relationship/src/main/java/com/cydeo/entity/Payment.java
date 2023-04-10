5d41c2c598d73812373e1fc5e7f7ae10c78a210f
Ivan_Cydeo <remezivan1@gmail.com>
2023-04-09 22:30:11 -0400

    SpringData TableRelations start

---
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Payment.java
@@ -0,0 +1,38 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Status;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Stack;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "payments")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate createdDate;
+    private BigDecimal amount;
+    @Enumerated(EnumType.STRING)
+    private Status paymentStatus;
+
+    @OneToOne // lets Spring know that One Payment can have only One PaymentDetail
+    @JoinColumn(name = "payment_detail_id")
+    // ^^ default name but can change foreign key column this way
+    private PaymentDetail paymentDetail;
+
+    public Payment(LocalDate createdDate, BigDecimal amount, Status paymentStatus) {
+        this.createdDate = createdDate;
+        this.amount = amount;
+        this.paymentStatus = paymentStatus;
+    }
+}
